3bad8250222ff05db84c200ba902c6bb2aebbd82
Nicholas Luiz <nicholasnkzambom@gmail.com>
2022-07-27 10:19:17 -0300

    Handling exception and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -3,6 +3,7 @@ package application;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import chess.ChessException;
 import chess.ChessPiece;
 import chess.ChessPosition;
 import chess.Color;
@@ -32,6 +33,10 @@ public class UI {
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
 	// https://stackoverflow.com/questions/2979383/java-clear-the-console
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
 
 	public static ChessPosition readChessPosition(Scanner ler) {
 		try {
@@ -39,7 +44,7 @@ public class UI {
 			char column = s.charAt(0);
 			int row = Integer.parseInt(s.substring(1));
 			return new ChessPosition(column, row);
-		} catch (RuntimeException e) {
+		} catch (ChessException e) {
 			throw new InputMismatchException("Erro reading ChessPosition. Valid values are from a1 to h8");
 		}
 	}
